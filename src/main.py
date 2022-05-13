bb7272bf6decc7621dd406e89b1ff34631511a4c
Minh N <minh123@outlook.com>
2022-05-13 16:41:52 -0700

    More docs, changed tilemap, and some other small changes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import arcade
 import pyglet
+from random import randint
 
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 TARGET_FPS = 60
+CAMERA_SPEED = 0.15  # The speed at which the camera moves to the player
 
 # Constants used to scale our sprites from their original size
 CHARACTER_SCALING = 4
@@ -19,7 +21,7 @@ PLAYER_JUMP_SPEED = 15
 MAX_SPEED = 5  # Speed limit
 ACCELERATION_RATE = 0.8 # How fast we accelearte
 FRICTION = 0.8 # How fast to slow down after we let off the key
-MAX_JUMP_COUNT = 10
+MAX_JUMP_COUNT = 8
 
 SPRITE_PIXEL_SIZE = 16
 GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
@@ -33,12 +35,14 @@ PLATFORMS_LAYER = "Platforms"
 MOVING_PLATFORMS_LAYER = "Moving Platforms"
 LADDERS_LAYER = "Ladders"
 COINS_LAYER = "Coins"
-FOREGROUND_LAYER = "Foreground"
-BACKGROUND_LAYER = "Background"
+#FOREGROUND_LAYER = "Foreground"
+#BACKGROUND_LAYER = "Background"
 DANGER_LAYER = "Dangers"
+GOAL_LAYER = "Goal"
 
 
 def load_texture_pair(filename):
+    """Load two textures from the filename, the first texture NOT flipped, the second texture flipped"""
     return [
         arcade.load_texture(file_name=filename),
         arcade.load_texture(file_name=filename, flipped_horizontally=True)
@@ -64,6 +68,7 @@ class Game(arcade.Window):
         self.gui_camera = None
         self.debug_text_y = 10
         self.jump_count = 0
+        self.moved_camera = False
 
         self.score = 0
         self.level = 1
@@ -78,15 +83,19 @@ class Game(arcade.Window):
         self.end_of_map = 0
 
         # Load sounds
-        resolve = arcade.resources.resolve_resource_path  # Just to make the function name shorter
-        self.collect_coin_sound = pyglet.media.load(resolve(":resources:sounds/coin1.wav"), streaming=False)
-        self.jump_sound = pyglet.media.load(resolve(":resources:sounds/jump1.wav"), streaming=False)
-        self.game_over_sound = pyglet.media.load(resolve(":resources:sounds/gameover1.wav"), streaming=False)
+        self.collect_coin_sound = self.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = self.load_sound(":resources:sounds/jump1.wav")
+        self.game_over_sound = self.load_sound(":resources:sounds/gameover1.wav")
+        self.goal_sound = self.load_sound(":resources:sounds/upgrade4.wav")
 
         arcade.enable_timings()
 
+    @staticmethod
+    def load_sound(path):
+        return pyglet.media.load(arcade.resources.resolve_resource_path(path), streaming=False)
+
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
+        """Set up the game here. Call this method to restart the game."""
 
         # Set up the Cameras
         self.camera = arcade.Camera(self.width, self.height)
@@ -110,6 +119,9 @@ class Game(arcade.Window):
                 "use_spatial_hash": True
             },
             DANGER_LAYER: {
+                "use_spatial_hash": True
+            },
+            GOAL_LAYER: {
                 "use_spatial_hash": True
             }
         }
@@ -152,7 +164,7 @@ class Game(arcade.Window):
         )
 
     def on_draw(self):
-        """Render the screen."""
+        """Clear, then render the screen."""
         
         # Clear the screen to the background color
         self.clear()
@@ -185,15 +197,17 @@ class Game(arcade.Window):
         self.debug_text("Jump Count", self.jump_count)
     
     def debug_text(self, item, value):
+        """Adds debug text to the top of the previous debug text."""
         text = f"{item}: {value}"
         arcade.draw_text(text=text, start_x=10, start_y=self.debug_text_y, color=arcade.csscolor.WHITE, font_size=18)
         self.debug_text_y += 30
 
     def reset_debug_text(self):
+        """Resets the debug text's position. You should call this every frame before you call self.debug_text()"""
         self.debug_text_y = 10
 
     def update_player_speed(self):
-        """Calculate speed based on the keys pressed."""
+        """Handle the player's movement based on the pressed keys."""
         if self.god_mode:
             # God mode physics
 
@@ -217,6 +231,7 @@ class Game(arcade.Window):
             if self.physics_engine.is_on_ladder():
                 if self.up_pressed and not self.down_pressed:
                     self.player.change_y = PLAYER_MOVEMENT_SPEED
+                    self.jump_count = MAX_JUMP_COUNT
                 elif self.down_pressed and not self.up_pressed:
                     self.player.change_y = -PLAYER_MOVEMENT_SPEED
                 else:
@@ -285,27 +300,43 @@ class Game(arcade.Window):
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = False
 
-    def center_camera_to_player(self):
+    def center_camera_to_player(self, camera_speed: float):
+        """Moved the camera to the player, specifically, the player's center position.
+
+        If self.moved_camera equals True, this method will do nothing. In order for it to work, you must reset
+        self.moved_camera to False.
+
+        camera_speed is the speed in which the camera moves to the player. It is passed to the arcade.Camera.move_to()
+        function
+        """
+        if self.moved_camera:
+            return
         screen_center_x = self.player.center_x - self.camera.viewport_width / 2
         screen_center_y = self.player.center_y - self.camera.viewport_height / 2
 
         # Dont let camera travel past 0
-        # if screen_center_x < 0: screen_center_x = 0
-        # if screen_center_y < 0: screen_center_y = 0
+        #if screen_center_x < 0: screen_center_x = 0
+        #if screen_center_y < 0: screen_center_y = 0
         player_centered = screen_center_x, screen_center_y
 
-        self.camera.move_to(player_centered, speed=0.2)
+        self.camera.move_to(player_centered, speed=camera_speed)
+        self.moved_camera = True
 
     def kill_player(self):
+        """Resets the player's position and kills the player."""
         self.player.stop()
         self.player.center_x = PLAYER_START_X
         self.player.center_y = PLAYER_START_Y
         self.game_over_sound.play()
+        # We want the camera to immediately arrive to the player, not slowly glide to the player
+        # So, we set camera_speed to 1.0 for that
+        self.center_camera_to_player(camera_speed=1.0)
 
     def on_update(self, delta_time):
         """Movement and game logic."""
         self.dt = delta_time * TARGET_FPS
         self.fps = 1 / delta_time
+        self.moved_camera = False
 
         # If god mode is enabled, set gravity to 0. This will allow the player to fly around.
         if self.god_mode:
@@ -319,10 +350,11 @@ class Game(arcade.Window):
         # See if we hit the coins
         coin_hit_list = arcade.check_for_collision_with_list(self.player, self.scene["Coins"])
 
+        # Better to do this than to play a sound for every single coin
         if coin_hit_list:
             self.collect_coin_sound.play()
             
-        # Loop through each coin if we hit (if any) and remove it
+        # Loop through each coin if we hit (if any) and remove it.
         for coin in coin_hit_list:
             coin.remove_from_sprite_lists()
             self.score += 1
@@ -332,14 +364,15 @@ class Game(arcade.Window):
            or self.player.center_y < -100:  # Did the player fall off the map?
             self.kill_player()
 
-        # See if the user got to the end of the level
-        if self.player.center_x >= self.end_of_map:
+        # See if the user reached the goal and finished the level.
+        if arcade.check_for_collision_with_list(self.player, self.scene[GOAL_LAYER]):
             # Advance to the next level
             self.level += 1
             # Load the next level
             self.setup()
+            self.goal_sound.play()
 
-        self.center_camera_to_player()
+        self.center_camera_to_player(camera_speed=CAMERA_SPEED)
         pyglet.clock.tick()
         pyglet.app.platform_event_loop.dispatch_posted_events()
         
